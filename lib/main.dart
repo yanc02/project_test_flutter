2b0172de5c0c7dcfbd39d68de1c2e6715ca191cd
Yan <theblack2darkness@gmail.com>
2019-08-22 17:08:41 -0400

    Working with frontEnd

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,96 @@
 import 'package:flutter/material.dart';
+import 'SecondRoute.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
-      home: MyHomePage(title: 'Loto Generator App'),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    title: 'Dominican Loto Generator App',
+//    theme: ThemeData(
+//      primarySwatch: Colors.yellow,
+//    ),
+    home: IndexView(),
+  ));
 }
 
-
-
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-
-}
-
-
-
-
-
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  String displayText;
-  String inputText;
-
-  void changeText(){
-    setState(() {
-      displayText = inputText;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class IndexView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Dominican Loto Generator App',
+            style: TextStyle(color: Colors.red)),
+        backgroundColor: Colors.yellow,
       ),
       body: Center(
-//        child: TextField(
-//          decoration: InputDecoration(
-//            border: InputBorder.none,
-//            hintText: 'Enter a search term'
+          child: Column(
+//            mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+//            decoration: new BoxDecoration(color: Colors.yellow),
+            child: Column(
+                children: <Widget>[
+                  Image.asset('images/loto_logo.jpg',
+                  width: 400,
+                  height: 150,
+                  fit:BoxFit.fill),
+
+//              RaisedButton(
+//                child: Text('VerifyNumbers'),
+//                onPressed: () {
+//                  Navigator.push(
+//                    context,
+//                    MaterialPageRoute(builder: (context) => SecondRoute()),
+//                  );
+//                },
+//              ),
+            ]),
+          ),
+          RaisedButton(
+            child: Text('Open route'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
+          ),
+//          RaisedButton(
+//            child: Text('Open route'),
+//            onPressed: () {
+//              Navigator.push(
+//                context,
+//                MaterialPageRoute(builder: (context) => SecondRoute()),
+//              );
+//            },
 //          ),
-//        )
-
-//        child: MaterialButton(
-//          color:Colors.red,
-//          child: Text(
-//              "Generate",
-//          style: TextStyle(
-//            fontSize: 20.0,
-//            color:Colors.black,
-//            ),
+//          RaisedButton(
+//            child: Text('Open route'),
+//            onPressed: () {
+//              Navigator.push(
+//                context,
+//                MaterialPageRoute(builder: (context) => SecondRoute()),
+//              );
+//            },
 //          ),
-//          shape: RoundedRectangleBorder(
-//              borderRadius: BorderRadius.circular(15.0)
+        ],
+
+
+
+
+
+
+//          child: RaisedButton(
+//            child: Text('Open route'),
+//            onPressed: () {
+//              Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),
+//              );
+//            },
 //          ),
-//          onPressed:() {},
-//        )
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "$displayText",
-            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-            TextField(
-              onChanged: (text){
-                inputText = text;
-              },
-              decoration: InputDecoration(
-
-                  border: OutlineInputBorder(),
-                  labelText: "Field Numbers",
-                  hintText: "Generated Numbers"
-              ),
-            ),
-
-            MaterialButton(
-              color: Colors.red,
-              child: Text(
-                "Generate",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.black,
-                ),
-              ),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0)),
-//              onPressed: () {},
-              onPressed: changeText,
-
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+//          child: RaisedButton(
+//              child: Text('Open reoute2'),
+//            onPressed: null,
+//
+//          )
+      )),
     );
   }
 }
